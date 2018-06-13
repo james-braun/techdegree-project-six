a7a055e6c7d93a00be41a3d9a415b249c21656a7
james braun <braun.james.robert@gmail.com>
2018-06-13 18:46:05 -0500

    removed javascript

--- projext six/js/JavaScript.js
+++ projext six/js/JavaScript.js
@@ -2,21 +2,3 @@ $('video').mediaelementplayer({
     features: ['playpause','progress','volume','fullscreen'],
     stretching: 'responsive'
 });
-const vid = document.getElementsByTagName("video");
-
-
-
-function myFunction(x) {
-    if (x.matches) {
-        vid[0].setAttribute('style', '"width: 960px; height: 540px;"');
-        //vid[0].setAttribute('height', '540px');
-        
-    } else {
-        vid[0].setAttribute('style', '"width: 480px; height: 270px;"');
-        //vid[0].setAttribute('height', '270px');
- 
-    }
-}
-var x = window.matchMedia("(min-width: 768px)");
-myFunction(x);
-x.addListener(myFunction);
