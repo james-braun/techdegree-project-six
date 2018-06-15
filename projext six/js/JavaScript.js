6157b42d50e445a4d489bdc2ddf80e5bb341e216
james braun <braun.james.robert@gmail.com>
2018-06-15 14:57:06 -0500

    fixed ie

--- projext six/js/JavaScript.js
+++ projext six/js/JavaScript.js
@@ -6,7 +6,7 @@ $('video').mediaelementplayer({
 const myVideo = document.getElementById('vid');
 const spanelement = document.querySelectorAll(".span-element");
 
-myVideo.addEventListener('timeupdate', () => {    
+myVideo.addEventListener('timeupdate', function() {    
     for (let i = 0; i < spanelement.length; i++) {
         if ((myVideo.currentTime > parseFloat($(spanelement[i]).attr("data-start"))) && (myVideo.currentTime < parseFloat($(spanelement[i]).attr("data-end")))) {
             spanelement[i].style.color = "blue";
@@ -16,8 +16,12 @@ myVideo.addEventListener('timeupdate', () => {
     }
 });
 
-for (let i = 0; i < spanelement.length; i++) {
-    spanelement[i].addEventListener('click', () => {
-        myVideo.currentTime = parseFloat($(spanelement[i]).attr("data-start"));
+for (let j = 0; j < spanelement.length; ++j) {
+    spanelement[j].addEventListener('click', function(e) {
+        for (let i = 0; i < spanelement.length; ++i) {
+            if ($(e.target).is(spanelement[i])) {
+                myVideo.currentTime = parseFloat($(spanelement[i]).attr("data-start"));    
+            }
+        }
     });
 }
--- projext six/js/internet-explorer.js
+++
@@ -1,8 +0,0 @@
-$(document.getElementsByTagName('video')).hide()
-$(document.getElementsByTagName('span')).hide()
-
-$('.wrapper').append('<h3>This page does not support internet explorer.</h3>');
-$('.wrapper').append('<h3>Please try one of these more modern browsers.</h3>');
-$('.wrapper').append('<h1><a href="https://www.microsoft.com/en-us/windows/microsoft-edge?&ocid=AID623741_SEM_jKcFnogZ" alt="edge browser">edge</a></h1>');
-$('.wrapper').append('<h1><a href="https://www.google.com/chrome/" alt="chrome browser">chrome</a></h1>');
-$('.wrapper').append('<h1><a href="https://www.mozilla.org/en-US/firefox/?utm_source=bing&utm_medium=cpc&utm_campaign=Firefox-Brand-US-Bing-Exact&utm_term=firefox&utm_content=A144_A634_A006547&gclid=CLnsyvTt1dsCFcHfDQodgk8KVQ&gclsrc=ds" alt="firefox browser">firefox</a></h1>');
