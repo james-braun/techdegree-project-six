e66500a05e7353304a1aa8f28150d5c6e3185ee6
james braun <braun.james.robert@gmail.com>
2018-06-14 19:42:54 -0500

    basic functionality

--- projext six/js/JavaScript.js
+++ projext six/js/JavaScript.js
@@ -2,3 +2,20 @@ $('video').mediaelementplayer({
     features: ['playpause','progress','volume','fullscreen'],
     stretching: 'responsive'
 });
+
+const vid = document.getElementById('vid');
+const spanelems = document.querySelectorAll(".span-element");
+
+vid.addEventListener('timeupdate', () => {
+    
+    for (let i = 0; i < spanelems.length; i++) {
+        if ((vid.currentTime > parseFloat($(spanelems[i]).attr("datastart"))) && (vid.currentTime < parseFloat($(spanelems[i]).attr("dataend")))) {
+            spanelems[i].style.color = "yellow";
+        } else {
+            spanelems[i].style.color = "black";
+        }
+        console.log(i);
+    }
+});
+
+ 
