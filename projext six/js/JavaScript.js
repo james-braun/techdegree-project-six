a0650ebf9a038f64ce9bc7f806486a42d36d2d72
james-braun <braun.james.robert@gmail.com>
2019-05-07 15:03:13 -0500

    changed varibles around and removed classes

--- projext six/js/JavaScript.js
+++ projext six/js/JavaScript.js
@@ -1,30 +1,24 @@
-// select video.
-const $myVideo = $('#vid');
-
-// select all span elements related to video.
-const $spanelement = $(".span-element")
-
 // style the video with medis element player plugin.
-$myVideo.mediaelementplayer({
+$('video').mediaelementplayer({
     features: ['playpause','progress','volume','fullscreen'],
     stretching: 'responsive'
 });
 
 // As the video plays highlight portions of th text.
-$myVideo.on('timeupdate', function () {   
-    for (let i = 0; i < $spanelement.length; ++i) {
-        if ((this.currentTime > parseFloat($spanelement.eq(i).attr("data-start"))) && (this.currentTime < parseFloat($spanelement.eq(i).attr("data-end")))) {
-            $spanelement.eq(i).css("color", "blue");
+$('video').on('timeupdate', function () {   
+    for (let i = 0; i < $('span').length; ++i) {
+        if ((this.currentTime > parseFloat($('span').eq(i).attr("data-start"))) && (this.currentTime < parseFloat($('span').eq(i).attr("data-end")))) {
+            $('span').eq(i).css("color", "blue");
         } else {
-            $spanelement.eq(i).css("color", "black");
+            $('span').eq(i).css("color", "black");
         }
     }
 });
 
 // If a portion of the text is clicked start playing
 // the video at that location
-for (let j = 0; j < $spanelement.length; ++j) {
-    $spanelement.eq(j).click(function() {
-        $myVideo[0].currentTime = $(this).attr("data-start");
+for (let j = 0; j < $('span').length; ++j) {
+    $('span').eq(j).click(function() {
+        $('video')[0].currentTime = $(this).attr("data-start");
     });
 }
